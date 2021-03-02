e4ab331efbb440a1b7b431a9b7672ce14e806d27
Ed <edwardmarkdixon.gmail.com>
2021-03-02 15:28:41 +0000

    fibonacci function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-'hello'
+function fibo(n){
+    if (n===0){
+        return 0
+    }
+    if (n===1){
+        return 1
+    }
+    else {
+        return fibo(n-1) + fibo(n-2)
+    }
+}
+
+function fibonacci(x){
+    for (i=1; i<=x; i++){
+        console.log(fibo(i))
+    }
+}
+fibonacci(10)
